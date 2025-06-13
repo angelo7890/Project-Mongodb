9bb8250f307ccaf97fd39eda786cc47ffd31b7de
angelo7890 <ar293978@gmail.com>
2025-06-13 10:58:49 -0300

    foi feita a configuracao das interfaces, repository e service no program.cs

--- lanchonete/Program.cs
+++ lanchonete/Program.cs
@@ -1,4 +1,8 @@
 using lanchonete.configuration;
+using lanchonete.interfaces;
+using lanchonete.repository;
+using lanchonete.service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
@@ -8,6 +12,17 @@ builder.Services.Configure<MongodbSettings>(builder.Configuration.GetSection("Mo
 builder.WebHost.UseUrls("http://localhost:8080");
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAdditionalRepository, AdditionalRepository>();
+builder.Services.AddScoped<IItemRepository,  ItemRepository>();
+builder.Services.AddScoped<IOrderRepository,  OrderRepository>();
+
+builder.Services.AddScoped<AdditionalService>();
+builder.Services.AddScoped<ItemService>();
+builder.Services.AddScoped<OrderService>();
+builder.Services.AddScoped<UserService>();
+
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
